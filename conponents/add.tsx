5e7c4303f2f547597dc5ec4e42934245c068fcd5
timDeHof <this.is.tim.dehof@gmail.com>
2023-05-21 11:16:42 -0400

    feat(add.tsx): create Add component to add movies to the database feat(addMovie.ts): create API endpoint to add movies to the database feat([id].tsx): create movie page to display movie details
    The changes add functionality to the application to allow users to add movies to the database. The Add component is created to provide a form for users to enter the IMDB ID of the movie they want to add. The AddMovie API endpoint is created to handle the addition of movies to the database. The [id].tsx page is created to display the details of a movie.

---
+++ conponents/add.tsx
@@ -0,0 +1,46 @@
+import { useRouter } from "next/router";
+import { useState } from "react";
+import Layout from "../components/layout";
+
+const Add = () => {
+  const [id, setImdbId] = useState("");
+  const router = useRouter();
+  const handleAddMovie = async () => {
+    try {
+      const response = await fetch(`/api/addMovie?imdbID=${id}`);
+      const data = await response.json();
+      if (response.status === 200 && data.id) {
+        console.log(
+          "Added the movie succesfully, redirecting to the movie page",
+        );
+        return router.push(`/movie/${data.id}`);
+      }
+      if (response.status === 409 && data.id) {
+        console.log("Movie already exists, redirecting to the movie page");
+        return router.push(`/movie/${data.id}`);
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  return (
+    <Layout>
+      <label className='flex flex-row space-x-2'>
+        <input
+          type='text'
+          className=' block w-1/2'
+          placeholder='Enter IMDB ID'
+          onChange={(e) => setImdbId(e.target.value)}
+        />
+        <button
+          className='bg-black p-2 px-4 text-lg text-white'
+          onClick={handleAddMovie}>
+          Add Movie
+        </button>
+      </label>
+    </Layout>
+  );
+};
+
+export default Add;
---
+++ pages/api/addMovie.ts
@@ -0,0 +1,61 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { initAppwrite } from "../../lib/appwrite";
+import Appwrite, { Query } from "node-appwrite";
+import { v4 as uuidv4 } from "uuid";
+
+type Data = {
+  data?: any;
+  id?: number;
+};
+const AddMovie = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+  try {
+    const imdbID = req.query.imdbID;
+    const sdk = initAppwrite();
+    let database = new Appwrite.Databases(sdk);
+
+    const response = await fetch(
+      `https://api.themoviedb.org/3/find/${imdbID}?api_key=${process.env.NEXT_PUBLIC_TMDB_MOVIE_KEY}&language=en-US&external_source=imdb_id`,
+    );
+    const data = await response.json();
+
+    if (data.movie_results.length > 0) {
+      const id = data.movie_results[0].id;
+      const item = data.movie_results[0];
+
+      const movieInDatabase = await database.listDocuments(
+        process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID as string,
+        process.env.NEXT_PUBLIC_APPWRITE_COLLECTION_ID as string,
+        [Query.equal("movie_id", id)],
+      );
+
+      if (movieInDatabase.documents.length > 0) {
+        // there are movies present in the database;
+        return res.status(409).json({ data: "Movie already exists", id });
+      } else {
+        // add the movie to the database
+        await database.createDocument(
+          process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID as string,
+          process.env.APPWRITE_COLLECTION_ID as string,
+          uuidv4(),
+          {
+            movie_id: id,
+            title: item.title,
+            thumbnail_image: `https://image.tmdb.org/t/p/original/${item.poster_path}`,
+            popularity: item.popularity,
+            release_date: item.release_date,
+            vote_average: item.vote_average,
+          },
+        );
+
+        return res.status(200).json({ id });
+      }
+    } else {
+      console.log("it is not a movie");
+    }
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({ data: e });
+  }
+};
+
+export default AddMovie;
