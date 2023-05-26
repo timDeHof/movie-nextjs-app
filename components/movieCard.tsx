0ac0d2d91a3869bbbf46b0ab3e8b43b66c01ff59
timDeHof <this.is.tim.dehof@gmail.com>
2023-05-25 21:42:18 -0400

    chore: clean up unused code and add MovieType interface to types/index.ts
    feat(movietype.ts): add 'MovieType' interface to describe the structure of a movie item.

--- components/movieCard.tsx
+++ components/movieCard.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import React from "react";
 import Image from "next/image";
+
 type MovieCardProps = {
   movie: {
     id: number;
---
+++ types/index.ts
@@ -0,0 +1,1 @@
+import { MovieType } from "./movietype";
