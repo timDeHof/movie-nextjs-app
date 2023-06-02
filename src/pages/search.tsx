1e8bb5cb061268d3b069abe5ad74598d189f1f4e
timDeHof <this.is.tim.dehof@gmail.com>
2023-06-01 22:43:36 -0400

    feat(pages): add no movies image and message to login prompt
    Add a new image and message to the login prompt in the search and watchlist pages. The message and image will be displayed when the user is not logged in.

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,10 +1,12 @@
 import type { NextPage } from "next";
+import Image from "next/image";
 import { useRouter } from "next/router";
 import { useState } from "react";
 import Layout from "src/components/layout";
 import MovieCard from "src/components/movieCard";
 import { SearchButton } from "src/components/button";
 import { useAppwrite } from "@providers/appwriteProvider";
+import noMoviesImage from "../../assets/no-movies-Icon.png";
 const Search: NextPage = () => {
   const [query, setQuery] = useState("");
   const [allMovies, setAllMovies] = useState([]);
@@ -41,7 +43,14 @@ const Search: NextPage = () => {
           </div>
         </div>
       ) : (
-        <p>Please login</p>
+        <div className='flex min-h-screen flex-col justify-center bg-gray-100 py-12 sm:px-6 lg:px-8'>
+          <div className='flex flex-col items-center sm:mx-auto sm:w-full sm:max-w-md'>
+            <Image src={noMoviesImage} alt='no movies icon'></Image>
+            <h2 className='mt-6 text-center text-3xl font-extrabold text-gray-900'>
+              Please log in to search for movies.
+            </h2>
+          </div>
+        </div>
       )}
     </Layout>
   );
--- src/pages/watchlist.tsx
+++ src/pages/watchlist.tsx
@@ -63,7 +63,14 @@ const Watchlist: NextPage = React.memo(() => {
   if (!isLoggedIn) {
     return (
       <Layout>
-        <p>Please login to view the watchlist </p>
+        <div className='flex min-h-screen flex-col justify-center bg-gray-100 py-12 sm:px-6 lg:px-8'>
+          <div className='flex flex-col items-center sm:mx-auto sm:w-full sm:max-w-md'>
+            <Image src={noMoviesImage} alt='no movies icon'></Image>
+            <h2 className='mt-6 text-center text-3xl font-extrabold text-gray-900'>
+              Please log in to access the watchlist.
+            </h2>
+          </div>
+        </div>
       </Layout>
     );
   }
