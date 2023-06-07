2e3d20785d8d21672805af827930f9df0aa14b14
timDeHof <this.is.tim.dehof@gmail.com>
2023-06-06 22:44:13 -0400

    feat(search): add infinite scroll to load more movies feat(search): add counts for total pages and total results feat(search): add search term to display search results fix(search): fix search term state variable name from query to searchTerm fix(search): fix endpoint url to include currentPage parameter in searchMovies API call fix(search): fix allMovies state variable to only store movie results feat(search): add title and hr to display search term and separate movie cards

--- src/pages/api/searchMovies.ts
+++ src/pages/api/searchMovies.ts
@@ -1,7 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import axios from "axios";
-import { client, databases } from "../../../lib/appwrite";
-import { v4 as uuidv4 } from "uuid";
 
 type Data = {
   data?: any;
@@ -10,14 +8,24 @@ type Data = {
 export default async (req: NextApiRequest, res: NextApiResponse<Data>) => {
   try {
     const query = req.query.query;
+    const currentPage = req.query.currentPage;
 
     const response = await axios(
-      `https://api.themoviedb.org/3/search/movie?query=${query}&api_key=${process.env.NEXT_PUBLIC_TMDB_MOVIE_KEY}&include_adult=false&language=en-US&page=1`,
+      `https://api.themoviedb.org/3/search/movie?&api_key=${process.env.NEXT_PUBLIC_TMDB_MOVIE_KEY}&include_adult=false&language=en-US&page=${currentPage}&query=${query}`,
     );
     const data = await response.data;
+    console.log("response:", data);
     if (data.results.length > 0) {
       const movies = data.results;
-      return res.status(200).json({ data: movies });
+      const totalPages = data.total_pages;
+      const totalResults = data.total_results;
+      return res.status(200).json({
+        data: {
+          results: movies,
+          totalPages: totalPages,
+          totalResults: totalResults,
+        },
+      });
     } else {
       console.log("it is not a movie");
     }
