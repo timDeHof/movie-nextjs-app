959710963a81a46fb93fe6ab92ddf2a60b8d6d8f
timDeHof <this.is.tim.dehof@gmail.com>
2023-06-10 15:05:48 -0400

    chore(eslint): add @typescript-eslint and react-hooks plugins, update rules chore(github): add CI workflow chore(package.json): add husky and format script chore(public): update site.webmanifest chore(tailwind.config.js): add font family to extend fix(appwrite.json): fix formatting fix(package.json): add @next/eslint-plugin-next

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,23 +3,26 @@ module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["var(--font-inter)"],
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
         "header-image":
           "url('https://images.unsplash.com/photo-1628432136678-43ff9be34064?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=363&q=100')",
-        "backgroundPosition": {
-          "bottom": "bottom",
+        backgroundPosition: {
+          bottom: "bottom",
           "bottom-4": "center bottom 1rem",
-          "center": "center ",
-          "left": "left",
+          center: "center ",
+          left: "left",
           "left-bottom": "left bottom",
           "left-top": "left top",
-          "right": "right",
+          right: "right",
           "right-bottom": "right bottom",
           "right-top": "right top",
-          "top": "top",
+          top: "top",
           "top-4": "center top 1rem",
         },
       },
