5cb72bb6fbf8e294c420a39d81beee89204b349b
timDeHof <this.is.tim.dehof@gmail.com>
2023-06-10 19:19:38 -0400

    style(layout.tsx): fix formatting and indentation feat(layout.tsx): add Signin and Register link to navigation menu feat(signinAndRegister.tsx): create Signin and Register page

--- src/pages/signinAndRegister.tsx
+++ src/pages/signinAndRegister.tsx
@@ -1,14 +1,11 @@
-// Import the required modules that are needed for displaying the movie list and pagination.
-import Layout from "src/components/layout";
+import Layout from "@components/layout";
+import React from "react";
 import Login from "src/components/login";
 import Register from "src/components/register";
-import React from "react";
-// Define the Home component as a functional React component with NextJS's 'NextPage' type.
-const Home = () => {
+export const signinAndRegister = () => {
   return (
     <Layout>
       <div className='flex h-screen w-full flex-col items-center'>
-        <h1 className='mb-6'>Homepage</h1>
         <div
           className='container w-full justify-center space-y-4 lg:flex
         lg:items-center lg:space-x-16 lg:space-y-0'>
@@ -19,5 +16,3 @@ const Home = () => {
     </Layout>
   );
 };
-
-export default Home;
