fae19222fe1e633fc832e8a8b16a5ee2beaa17c1
timDeHof <this.is.tim.dehof@gmail.com>
2023-06-10 21:47:16 -0400

    style(signinAndRegister.tsx): replace inline comments with JSX comments

--- src/pages/signinAndRegister.tsx
+++ src/pages/signinAndRegister.tsx
@@ -8,9 +8,10 @@ const SignInAndRegister = () => {
       <div
         className='container h-screen w-full justify-center space-y-4 lg:flex
         lg:items-center lg:space-x-16 lg:space-y-0'>
-        // Render the Login component inside the container element.
+        {/* Render the Login component inside the container element. */}
         <Login />
-        // Render the Register component inside the container element.
+        {/* // Render the Register component inside the container element. */}
+        <Register />
       </div>
     </Layout>
   );
