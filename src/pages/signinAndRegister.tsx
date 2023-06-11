c2645270a16af81a1932677286e594e5d8d1963e
timDeHof <this.is.tim.dehof@gmail.com>
2023-06-10 21:34:24 -0400

    refactor(signinAndRegister.tsx): change function name to follow PascalCase convention and fix indentation feat(signinAndRegister.tsx): adjust container height to full screen

--- src/pages/signinAndRegister.tsx
+++ src/pages/signinAndRegister.tsx
@@ -2,18 +2,16 @@ import Layout from "@components/layout";
 import React from "react";
 import Login from "@components/login";
 import Register from "@components/register";
-const signInAndRegister = () => {
+const SignInAndRegister = () => {
   return (
     <Layout>
-      <div className='flex h-screen w-full flex-col items-center'>
-        <div
-          className='container w-full justify-center space-y-4 lg:flex
+      <div
+        className='container h-screen w-full justify-center space-y-4 lg:flex
         lg:items-center lg:space-x-16 lg:space-y-0'>
-          <Login />
-          <Register />
-        </div>
+        <Login />
+        <Register />
       </div>
     </Layout>
   );
 };
-export default signInAndRegister;
+export default SignInAndRegister;
