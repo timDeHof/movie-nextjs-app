bc1659fafb55eb926b489e6a2a3a621ae88a5965
timDeHof <this.is.tim.dehof@gmail.com>
2023-09-29 16:08:15 -0400

    chore(package.json): update dependencies versions fix(index.tsx): optimize rendering performance by memoizing MovieCard component The package.json file has been updated to include the latest versions of the dependencies. This ensures that the project is using the most up-to-date and stable versions of the packages.
    In the index.tsx file, the MovieCard component has been memoized using React's memo function. This optimizes the rendering performance by preventing unnecessary re-renders of the MovieCard component when the parent component re-renders.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 // Import the required modules that are needed for displaying the movie list and pagination.
 import axios from "axios";
 import Layout from "src/components/layout";
-import React from "react";
+import React, {memo} from "react";
 import { Movies } from "@typings/search.types";
 import MovieCard from "@components/movieCard";
+
+const API_URL = `https://api.themoviedb.org/3/movie/popular?&api_key=${process.env.NEXT_PUBLIC_TMDB_MOVIE_KEY}&include_adult=false&language=en-US&page=1`
 interface MovieProps {
   movies: Movies[];
 }
@@ -11,36 +13,35 @@ interface MovieProps {
 const Home = ({ movies }: MovieProps) => {
   return (
     <Layout>
-      <div className='flex h-screen w-full flex-1 flex-col items-center text-center lg:px-20'>
+      <div className='flex flex-col items-center flex-1 w-full h-screen text-center lg:px-20'>
         <h1 className='text-4xl font-bold lg:text-6xl'>Welcome to ReelWatch</h1>
         <p className='my-3 text-2xl'>
           Keep track of your favorite movies and discover new ones.
         </p>
-        <div className='mt-6 grid gap-4 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-4'>
-          {movies.slice(0, 4).map((movie, id) => (
-            <MovieCard movie={movie} key={id} />
+        <div className='grid gap-4 mt-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-4'>
+          {movies.slice(-4).map((movie:Movies) => (
+            <MemoizedMovieCard movie={movie} key={movie.id} />
           ))}
         </div>
       </div>
     </Layout>
   );
 };
+
+const MemoizedMovieCard = memo(MovieCard)
 export async function getServerSideProps(context: {
   query: { id: string };
 }): Promise<{ props: MovieProps }> {
   const { id } = context.query;
   try {
-    const response = await axios(
-      `https://api.themoviedb.org/3/movie/popular?&api_key=${process.env.NEXT_PUBLIC_TMDB_MOVIE_KEY}&include_adult=false&language=en-US&page=1`,
-    );
-
+    const response = await axios(API_URL);
     const data = await response.data;
-    console.log(data.results);
+
     return {
       props: {
         movies: data.results,
       },
-    };
+    }
   } catch (error) {
     console.error(error);
     throw new Error("Failed to fetch movie data");
