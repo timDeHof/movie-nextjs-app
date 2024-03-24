04a3b3fb39dc52f4dd69b581602269ba3d9043cd
timDeHof <ttdehof@gmail.com>
2024-03-24 13:55:17 -0400

    feat(watchlistItem.tsx): add support for removing movies from watchlist fix(register.tsx): fix error message when unable to create account fix(login.tsx): fix error message when invalid email or password fix(layout.tsx): improve code readability and add support for process.env.PORT fix(server.ts): change port variable case from lowercase port to uppercase PORT to improve semantics The changes in watchlistItem.tsx add support for removing movies from the watchlist. The changes in register.tsx fix the error message when unable to create an account. The changes in login.tsx fix the error message when an invalid email or password is entered. The changes in layout.tsx improve code readability and add support for the process.env.PORT environment variable. The changes in server.ts change the port variable case from lowercase port to uppercase PORT to improve semantics.

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,121 +1,129 @@
 // Importing the 'Inter' font from the 'next/font/google' library
-import { Inter } from "next/font/google";
+
+// Importing an SVG logo file as a variable named 'reelLogo'
+import React, { useEffect } from 'react';
+import { Inter } from 'next/font/google';
+import Head from 'next/head';
+import Image from 'next/image';
+import Link from 'next/link';
+import { useAtom } from 'jotai';
+
+import reelLogo from '@/assets/film-reel-svgrepo-com.svg';
+import { isLoggedInAtom } from '@/atoms/user';
+import { client } from '@/lib/appwrite';
+import { useUser } from '@/providers/userProvider';
+
+import { Routes } from '@/config/routes';
 
 // Initializing the 'Inter' font object with a subset of "latin"
 const inter = Inter({
-  subsets: ["latin"],
-  variable: "--font-inter",
+  subsets: ['latin'],
+  variable: '--font-inter',
 });
-import React, { useEffect } from "react";
-import { useAtom } from "jotai";
-// Importing necessary components from the 'next/link', 'next/head', and 'next/image' libraries.
-import Link from "next/link";
-import Head from "next/head";
-import Image from "next/image";
-import { Routes } from "../config/routes";
-
-import { useUser } from "@providers/userProvider";
-import { client } from "@/lib/appwrite";
-// Importing an SVG logo file as a variable named 'reelLogo'
-import reelLogo from "../../assets/film-reel-svgrepo-com.svg";
-import { isLoggedInAtom } from "src/atoms/user";
 
 interface LayoutProps {
   children: React.ReactNode;
 }
 
 const Layout = ({ children }: LayoutProps) => {
-    const user = useUser();
+  const user = useUser();
   const [isLoggedIn, setIsLoggedIn] = useAtom(isLoggedInAtom);
-useEffect(() => {
-  // Check if session token exists in cookie or local storage
-  const sessionToken =
-    localStorage.getItem("sessionToken") ??
-    document.cookie.replace(
-      /(?:(?:^|.*;\s*)sessionToken\s*\=\s*([^;]*).*$)|^.*$/,
-      "$1",
-    );
-  if (sessionToken) {
-    // Set session using stored token
-    client.setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT as string);
-    client.setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID as string);
-    client.setJWT(sessionToken);
-    setIsLoggedIn(true);
-  }
-}, [client]);
+  useEffect(() => {
+    // Check if session token exists in cookie or local storage
+    const sessionToken =
+      localStorage.getItem('sessionToken') ??
+      document.cookie.replace(
+        /(?:(?:^|.*;\s*)sessionToken\s*\=\s*([^;]*).*$)|^.*$/,
+        '$1',
+      );
+    if (sessionToken) {
+      // Set session using stored token
+      client.setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT as string);
+      client.setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID as string);
+      client.setJWT(sessionToken);
+      setIsLoggedIn(true);
+    }
+  }, [client]);
 
   return (
     <>
       {/* The header meta tags */}
       <Head>
         <link
-          rel='apple-touch-icon'
-          sizes='180x180'
-          href='/images/apple-touch-icon.png'></link>
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/images/apple-touch-icon.png"
+        ></link>
         <link
-          rel='icon'
-          type='image/png'
-          sizes='32x32'
-          href='/images/favicon-32x32.png'></link>
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/images/favicon-32x32.png"
+        ></link>
         <link
-          rel='icon'
-          type='image/png'
-          sizes='16x16'
-          href='/images/favicon-16x16.png'></link>
-        <link rel='icon' href='/images/favicon.ico' sizes='any'></link>
-        <link rel='manifest' href='/images/site.webmanifest'></link>
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/images/favicon-16x16.png"
+        ></link>
+        <link rel="icon" href="/images/favicon.ico" sizes="any"></link>
+        <link rel="manifest" href="/images/site.webmanifest"></link>
         <title>ReelWatch</title>
       </Head>
       {/* The header section */}
-      <header className='w-screen text-sky-800'>
+      <header className="w-screen text-sky-800">
         <div
-          className='flex w-screen flex-col flex-wrap items-center
+          className="flex w-screen flex-col flex-wrap items-center
         bg-header-image bg-cover bg-[center_20%] px-20 py-8 opacity-70
-        bg-blend-multiply md:flex-row'>
+        bg-blend-multiply md:flex-row"
+        >
           {/* The application logo */}
-          <Link rel='preconnect' href={Routes.home}>
-            <div className='flex flex-row max-w-xs space-x-2'>
-              <Image src={reelLogo} alt={"logo"} width={32} height={32} />
+          <Link rel="preconnect" href={Routes.home}>
+            <div className="flex max-w-xs flex-row space-x-2">
+              <Image src={reelLogo} alt={'logo'} width={32} height={32} />
               {/* The branding text which consists of the words 'Reel' and 'Watch' */}
-              <div className='flex items-end justify-center lg:border-l-black'>
-                <p className='flex items-center justify-center text-2xl font-medium text-white cursor-pointer md:mb-0'>
+              <div className="flex items-end justify-center lg:border-l-black">
+                <p className="flex cursor-pointer items-center justify-center text-2xl font-medium text-white md:mb-0">
                   Reel
                 </p>
-                <span className='flex items-end text-xl font-medium cursor-pointer text-sky-800'>
+                <span className="flex cursor-pointer items-end text-xl font-medium text-sky-800">
                   Watch
                 </span>
               </div>
             </div>
           </Link>
           {/* The navigation menu */}
-          <nav className='flex flex-wrap items-center justify-center text-base md:ml-4 md:mr-auto md:border-l md:border-gray-400 md:py-1 md:pl-4'>
-            <span className='mr-5 hover:text-sky-900'>
+          <nav className="flex flex-wrap items-center justify-center text-base md:ml-4 md:mr-auto md:border-l md:border-gray-400 md:py-1 md:pl-4">
+            <span className="mr-5 hover:text-sky-900">
               <Link href={Routes.search}>Find Movies</Link>
             </span>
-            <span className='mr-5 hover:text-sky-900'>
-              <Link rel='preconnect' href={Routes.watchList}>
+            <span className="mr-5 hover:text-sky-900">
+              <Link rel="preconnect" href={Routes.watchList}>
                 My Watchlist
               </Link>
             </span>
           </nav>
           {isLoggedIn ? (
             <button
-              className='p-2 px-4 text-sm text-white uppercase rounded-lg bg-sky-800 hover:bg-sky-900'
-              onClick={() => user?.logout()}>
+              className="rounded-lg bg-sky-800 p-2 px-4 text-sm uppercase text-white hover:bg-sky-900"
+              onClick={() => user?.logout()}
+            >
               logout
             </button>
           ) : (
-            <div className='flex justify-around w-full lg:w-2/12'>
+            <div className="flex w-full justify-around lg:w-2/12">
               <Link
-                className='p-2 px-4 text-sm text-white uppercase rounded-lg bg-sky-800 hover:bg-sky-900'
-                rel='preconnect'
-                href={Routes.login}>
+                className="rounded-lg bg-sky-800 p-2 px-4 text-sm uppercase text-white hover:bg-sky-900"
+                rel="preconnect"
+                href={Routes.login}
+              >
                 Login
               </Link>
               <Link
-                className='p-2 px-4 text-sm text-white uppercase rounded-lg bg-sky-800 hover:bg-sky-900'
-                rel='preconnect'
-                href={Routes.register}>
+                className="rounded-lg bg-sky-800 p-2 px-4 text-sm uppercase text-white hover:bg-sky-900"
+                rel="preconnect"
+                href={Routes.register}
+              >
                 Register
               </Link>
             </div>
--- src/components/movieCard.tsx
+++ src/components/movieCard.tsx
@@ -1,9 +1,11 @@
-import Link from "next/link";
-import React from "react";
-import Image from "next/image";
-import { WatchButton } from "./button";
-import { useRouter } from "next/router";
-import { useAppwrite } from "@providers/appwriteProvider";
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
+import { useAppwrite } from '@/providers/appwriteProvider';
+import { WatchButton } from './button';
+
 type MovieCardProps = {
   movie: {
     id: number;
@@ -24,12 +26,12 @@ const MovieCard: React.FC<MovieCardProps> = ({ movie }) => {
       const data = await response.json();
       if (response.status === 200 && data.id) {
         console.log(
-          "Added the movie succesfully, redirecting to the movie page",
+          'Added the movie succesfully, redirecting to the movie page',
         );
         return router.push(`/search`);
       }
       if (response.status === 409 && data.id) {
-        console.log("Movie already exists, redirecting to the movie page");
+        console.log('Movie already exists, redirecting to the movie page');
         return router.push(`/search`);
       }
     } catch (e) {
@@ -38,35 +40,32 @@ const MovieCard: React.FC<MovieCardProps> = ({ movie }) => {
   };
 
   return (
-    <div className='flex h-full max-h-[675px] flex-col'>
-      <Link className='flex-1' rel='preconnect' href={`/movie/${movie.id}`}>
-        <div className='grid max-w-xs grid-cols-1 overflow-hidden rounded shadow-lg duration-300 ease-in-out hover:scale-105'>
+    <div className="flex h-full max-h-[675px] flex-col">
+      <Link className="flex-1" rel="preconnect" href={`/movie/${movie.id}`}>
+        <div className="grid max-w-xs grid-cols-1 overflow-hidden rounded shadow-lg duration-300 ease-in-out hover:scale-105">
           <Image
             src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
             alt={movie.title}
             width={500}
             height={460}
           />
-          <div className='col-end-auto space-y-3 p-2'>
-            <p className='text-sm font-bold'>{movie.title}</p>
+          <div className="col-end-auto space-y-3 p-2">
+            <p className="text-sm font-bold">{movie.title}</p>
             <p
               className={
                 isLoggedIn
-                  ? "line-clamp-3 text-xs text-gray-600"
-                  : "line-clamp-3 text-sm text-gray-600"
-              }>
-              {movie.overview.split(". ", 1) + "."}
+                  ? 'line-clamp-3 text-xs text-gray-600'
+                  : 'line-clamp-3 text-sm text-gray-600'
+              }
+            >
+              {movie.overview.split('. ', 1) + '.'}
             </p>
             {isLoggedIn && (
-              <div
-                className='flex flex-col content-end lg:flex-row
-            lg:justify-between'>
-                <span
-                  className='flex items-center justify-center rounded-lg bg-gray-200 px-2
-                text-xs font-semibold text-gray-700'>
+              <div className="flex flex-col content-end lg:flex-row lg:justify-between">
+                <span className="flex items-center justify-center rounded-lg bg-gray-200 px-2 text-xs font-semibold text-gray-700">
                   {movie.release_date}
                 </span>
-                <WatchButton onClick={handleAddMovie} text='watchlist' />
+                <WatchButton onClick={handleAddMovie} text="watchlist" />
               </div>
             )}
           </div>
